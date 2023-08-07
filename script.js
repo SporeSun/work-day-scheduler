18f1c13c1cf2bb16c50e39fda9fbad9782357bfa
Shiny Ballzach <135516150+SporeSun@users.noreply.github.com>
2023-08-06 19:00:15 -0700

    Final formatting edits

--- script.js
+++ script.js
@@ -1,56 +1,53 @@
-
 $( document ).ready(function () {
-  $(function () {
-    // TODO: Add code to display the current date in the header of the page.
-    // Display the current day at the top of the calendar
+  // TODO: Add code to display the current date in the header of the page.
+  // Display the current day at the top of the calendar
 
-    $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
 
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    // Function to color code the time blocks based on past, present, or future
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  // Function to color code the time blocks based on past, present, or future
+  var currentHour = dayjs().hour();
+  function updateTimeBlocks() {
     var currentHour = dayjs().hour();
-    function updateTimeBlocks() {
-      var currentHour = dayjs().hour();
-  
-      $(".time-block").each(function () {
-        var blockHour = parseInt($(this).attr("id").split("-")[1]);
-  
-        if (blockHour < currentHour) {
-          $(this).removeClass("present future").addClass("past");
-        } else if (blockHour === currentHour) {
-          $(this).removeClass("past future").addClass("present");
-        } else {
-          $(this).removeClass("past present").addClass("future");
-        }
-      });
-    }
-      
-    // Call the function to update time block colors
-    updateTimeBlocks();
+
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      if (blockHour < currentHour) {
+        $(this).removeClass("present future").addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past future").addClass("present");
+      } else {
+        $(this).removeClass("past present").addClass("future");
+      }
+    });
+  }
     
-
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-
-    // Load saved events from local storage
-    function loadEvents() {
-      $(".time-block").each(function () {
-        var eventId = $(this).attr("id");
-        var eventText = localStorage.getItem(eventId);
+  // Call the function to update time block colors
+  updateTimeBlocks();
   
-        if (eventText) {
-          $(this).find(".description").val(eventText);
-        }
-      });
-    }
+
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+
+  // Load saved events from local storage
+  function loadEvents() {
+    $(".time-block").each(function () {
+      var eventId = $(this).attr("id");
+      var eventText = localStorage.getItem(eventId);
+
+      if (eventText) {
+        $(this).find(".description").val(eventText);
+      }
+  });
   
     // Call the function to load events from local storage
-    loadEvents();
+loadEvents();
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -65,6 +62,4 @@ $( document ).ready(function () {
   
       localStorage.setItem(eventId, eventText);
     });
-  });
-  });
-
+});
